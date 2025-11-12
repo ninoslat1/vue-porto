0e1f8341c827d0957f80f66c3bb16f0562650ecc
ninoslat1 <benedict.ellnina@gmail.com>
2025-11-12 23:28:59 +0700

    feat: add shadcn vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,35 @@
+<script setup lang="ts">
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from '@/components/ui/menubar'
+</script>
+
 <template>
-    <h1>ben.dev</h1>
+    <div class="flex gap-5 w-full">
+        <div class="flex gap-5 items-center justify-around w-1/4">
+            <!-- <i class="pi pi-arrow-right-arrow-left"></i>
+            <p>ben.dev</p>
+            <p>Home</p>
+            <p>LinkedIn</p>
+            <p>Resume</p> -->
+            <Menubar>
+                <MenubarMenu>
+                    <MenubarTrigger>ben.dev</MenubarTrigger>
+                    <MenubarTrigger>File</MenubarTrigger>
+                    <MenubarTrigger>Home</MenubarTrigger>
+                    <MenubarTrigger>LinkedIn</MenubarTrigger>
+                    <MenubarTrigger>Resume</MenubarTrigger>
+                </MenubarMenu>
+            </Menubar>
+        </div>
+        <div>
+
+        </div>
+    </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    tsconfigPaths()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
